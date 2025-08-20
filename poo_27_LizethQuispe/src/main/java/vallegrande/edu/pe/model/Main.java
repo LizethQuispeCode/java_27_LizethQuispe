b1f38b6f3521712e144659268010687f6a00c5cc
Lizeth Quispe <lizeth.quispe@vallegrande.edu.pe>
2025-08-20 02:57:12 -0500

    actualización

--- poo_27_LizethQuispe/src/main/java/vallegrande/edu/pe/model/Main.java
+++ poo_27_LizethQuispe/src/main/java/vallegrande/edu/pe/model/Main.java
@@ -4,27 +4,27 @@ package vallegrande.edu.pe.model;
 public class Main {
 
     public static void main(String[] args) {
-        // --- Prueba para ejercicio1 (Carro) ---
-        System.out.println("--- Prueba para ejercicio1 ---");
+
+        System.out.println("--- Prueba para ejercicio2 ---");
         Ejercicio2 miCarro = new Ejercicio2("Toyota", "Corolla");
         miCarro.arrancar();
         System.out.println("\n");
 
-        // --- Prueba para ejercicio2 (Calculadora) ---
-        System.out.println("--- Prueba para ejercicio2 ---");
+
+        System.out.println("--- Prueba para ejercicio3 ---");
         Ejercicio3 miCalculadora = new Ejercicio3(10);
         double resultado = miCalculadora.sumar(5);
         System.out.println("El resultado de la suma es: " + resultado);
         System.out.println("\n");
 
-        // --- Prueba para ejercicio3 (Empleado) ---
-        System.out.println("--- Prueba para ejercicio3 ---");
+
+        System.out.println("--- Prueba para ejercicio4 ---");
         Ejercicio4 empleado1 = new Ejercicio4("Juan Pérez", "Desarrollador Junior");
         empleado1.actualizarCargo("Desarrollador Senior");
         System.out.println("\n");
 
-        // --- Prueba para ejercicio4 (Persona) ---
-        System.out.println("--- Prueba para ejercicio4 ---");
+      
+        System.out.println("--- Prueba para ejercicio5 ---");
         Ejercicio5 nuevaPersona = new Ejercicio5("María", 30);
         nuevaPersona.saludar();
     }
